648de861b71d11e11bc4e9446d46c3573cdfb3ff
Khalips <khaliphilepapiyana7@gmail.com>
2025-05-19 09:25:28 +0200

    Project Files

--- muscles.js
+++
@@ -1,7 +0,0 @@
-function showFact(type) {
-    // This could be expanded to show more detailed information
-    console.log(`Showing fact about ${type}`);
-    
-    // Example of displaying a modal or expanding the fact card
-    alert(`You clicked on the ${type} muscle fact!`);
-}
